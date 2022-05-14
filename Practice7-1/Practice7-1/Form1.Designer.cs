0a5c1668e7f294c9f1c7745497f74c82aab67ff8
githubアカウントのユーザー名 <github>
2022-05-14 19:44:31 +0900

    Practice7-1

---
+++ Practice7-1/Practice7-1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Practice7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.taxPriceBox = new System.Windows.Forms.TextBox();
+            this.taxBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(184, 43);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(285, 25);
+            this.priceBox.TabIndex = 0;
+            // 
+            // taxPriceBox
+            // 
+            this.taxPriceBox.Enabled = false;
+            this.taxPriceBox.Location = new System.Drawing.Point(184, 164);
+            this.taxPriceBox.Name = "taxPriceBox";
+            this.taxPriceBox.Size = new System.Drawing.Size(285, 25);
+            this.taxPriceBox.TabIndex = 1;
+            // 
+            // taxBox
+            // 
+            this.taxBox.Enabled = false;
+            this.taxBox.Location = new System.Drawing.Point(341, 105);
+            this.taxBox.Name = "taxBox";
+            this.taxBox.Size = new System.Drawing.Size(128, 25);
+            this.taxBox.TabIndex = 2;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(407, 228);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(126, 33);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "計算する";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.CalcButtonClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "税抜価格";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "税込価格";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "税";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "税率";
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(184, 229);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(132, 26);
+            this.ComboBox.TabIndex = 8;
+            this.ComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 285);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.taxBox);
+            this.Controls.Add(this.taxPriceBox);
+            this.Controls.Add(this.priceBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox taxPriceBox;
+        private System.Windows.Forms.TextBox taxBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox ComboBox;
+    }
+}
+
---
+++ Practice7-1/Practice7-1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice7_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //コンボボックス(ComboBox)へ項目を追加
+            ComboBox.Items.Add("0.1");
+            ComboBox.Items.Add("0.5");
+            ComboBox.Items.Add("0.8");
+            ComboBox.Items.Add("1.0");
+        }
+
+        public double tax;
+
+        private void CalcButtonClicked(object sender, EventArgs e)
+        {
+            int price;
+            int index = ComboBox.SelectedIndex;
+            bool success = int.TryParse(this.priceBox.Text, out price);
+
+            if (success == true)
+            {
+                // 消費税を計算する
+                int taxPrice = (int)(price * (tax + 1));
+                this.taxPriceBox.Text = String.Format("{0:#,0}",taxPrice).ToString();
+
+
+                // 税を計算する
+                  this.taxBox.Text = String.Format("{0:#,0}",taxPrice - price).ToString();
+
+            }
+
+            else
+            {
+                MessageBox.Show("入力が正しくありません");
+                priceBox.Text = "";
+            }
+        }
+
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tax = double.Parse(ComboBox.Text);
+        }
+    }
+}
